6cdf3eca62e697dc1eb517081fe3cb31fd19a48f
Harry Pidcock <harry.pidcock@canonical.com>
2021-03-11 10:32:10 +1000

    Migrate to Bases instead of Systems.

--- system.go
+++ system.go
@@ -4,8 +4,6 @@
 package systems
 
 import (
-	"fmt"
-	"regexp"
 	"strings"
 
 	"github.com/juju/errors"
@@ -13,106 +11,75 @@ import (
 	"github.com/juju/systems/channel"
 )
 
-// System represents an OS/Channel or Resource.
-// Systems can also be converted to and from a series string.
-type System struct {
-	OS       string          `json:"os,omitempty"`
-	Channel  channel.Channel `json:"channel,omitempty"`
-	Resource string          `json:"resource,omitempty"`
+// Base represents an OS/Channel.
+// Bases can also be converted to and from a series string.
+type Base struct {
+	Name    string          `json:"name,omitempty"`
+	Channel channel.Channel `json:"channel,omitempty"`
 }
 
-// Validate returns with no error when the System is valid.
-func (s System) Validate() error {
-	if s.OS == "" && s.Resource == "" {
-		return errors.NotValidf("one of os or resource must be specified")
+// Validate returns with no error when the Base is valid.
+func (s Base) Validate() error {
+	if s.Name == "" {
+		return errors.NotValidf("name must be specified")
 	}
 
-	if s.OS != "" {
-		if s.Resource != "" {
-			return errors.NotValidf("resource cannot be specified with os")
-		}
-		if !validOS.Contains(s.OS) {
-			return errors.NotValidf("os %q", s.OS)
-		}
-		if s.Channel == channel.Empty {
-			return errors.NotValidf("missing channel")
-		}
+	if !validOS.Contains(s.Name) {
+		return errors.NotValidf("os %q", s.Name)
+	}
+	if s.Channel == channel.Empty {
+		return errors.NotValidf("channel")
 	}
 
-	if s.Resource != "" {
-		if s.Channel != channel.Empty {
-			return errors.NotValidf("channel cannot be specified with resource")
-		}
-	}
 	return nil
 }
 
-// String respresentation of the System, used for series backwards compatability.
-func (s System) String() string {
+// String respresentation of the Base, used for series backwards compatability.
+func (s Base) String() string {
 	// Handle legacy series.
-	if series, ok := systemToSeries[s]; ok {
+	if series, ok := baseToSeries[s]; ok {
 		return series
 	}
-	// Handle new system as a series.
-	str := "system"
-	if s.OS != "" {
-		str += fmt.Sprintf("#os=%s", s.OS)
-	}
+	str := s.Name
 	if s.Channel != channel.Empty {
-		str += fmt.Sprintf("#channel=%s", s.Channel.String())
-	}
-	if s.Resource != "" {
-		str += fmt.Sprintf("#resource=%s", s.Resource)
+		str += "/" + s.Channel.String()
 	}
 	return str
 }
 
-// regex to match k=v from system series strings in the form
-// "system#os=ubuntu#version=18.04#resource=imagename"
-var systemSeriesRegex = regexp.MustCompile(`#(os|channel|resource)=([^#]+)`)
-
-// ParseSystemFromSeries matches legacy series like "focal" or parses a system as series string
-// in the form "system#os=ubuntu#version=18.04#resource=imagename"
-func ParseSystemFromSeries(s string) (System, error) {
+// ParseBaseFromSeries matches legacy series like "focal" or parses a base as series string
+// in the form "os/track/risk/branch"
+func ParseBaseFromSeries(s string) (Base, error) {
 	var err error
-	if !strings.HasPrefix(s, "system") {
-		system, ok := seriesToSystem[s]
-		if !ok {
-			return System{}, errors.NotValidf("series %s is unsupported", s)
-		}
-		return system, nil
+	if base, ok := seriesToBases[s]; ok {
+		return base, nil
 	}
-	propString := strings.TrimPrefix(s, "system")
-	matches := systemSeriesRegex.FindAllStringSubmatch(propString, -1)
-	if len(matches) == 0 {
-		return System{}, errors.NotValidf("invalid system series string %q", s)
+
+	// Split the first forward-slash to get name and channel.
+	// E.g. "os/track/risk/branch" => ["os", "track/risk/branch"]
+	segments := strings.SplitN(s, "/", 2)
+	osName := segments[0]
+	channelName := ""
+	if len(segments) == 2 {
+		channelName = segments[1]
 	}
-	matchedCharacters := 0
-	system := System{}
-	for _, v := range matches {
-		matchedCharacters += len(v[0])
-		key := v[1]
-		value := v[2]
-		switch key {
-		case "os":
-			system.OS = value
-		case "channel":
-			system.Channel, err = channel.Parse(value)
-			if err != nil {
-				return System{}, errors.Annotatef(err, "invalid channel %q", value)
-			}
-		case "resource":
-			system.Resource = value
-		default:
-			return System{}, errors.NotValidf("key %q in system series string %q", key, s)
+
+	base := Base{}
+	if !validOS.Contains(osName) {
+		return Base{}, errors.NotValidf("series %q", s)
+	}
+	base.Name = osName
+
+	if channelName != "" {
+		base.Channel, err = channel.Parse(channelName)
+		if err != nil {
+			return Base{}, errors.Annotatef(err, "malformed channel in base string %q", s)
 		}
 	}
-	if matchedCharacters != len(propString) {
-		return System{}, errors.NotValidf("system series string %q", s)
-	}
-	err = system.Validate()
+
+	err = base.Validate()
 	if err != nil {
-		return System{}, errors.Annotatef(err, "invalid system series string %q", s)
+		return Base{}, errors.Annotatef(err, "invalid base string %q", s)
 	}
-	return system, nil
+	return base, nil
 }
